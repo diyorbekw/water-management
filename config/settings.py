4b1eecfa09d8bd7c6c670429c3c35c7a0d55a1ff
Diyorbek <diyorbek0143@gmail.com>
2026-01-05 17:33:21 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -183,7 +183,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "https://water-management.sifatdev.uz",
 ]
-
+ 
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
