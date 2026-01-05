a7ec97b658f9a6913c0e1cb0fff7c1765f2aae68
Diyorbek <diyorbek0143@gmail.com>
2026-01-05 17:31:25 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -173,3 +173,23 @@ SPECTACULAR_SETTINGS = {
     'COMPONENT_SPLIT_CREATE': True,
 
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://water-management.sifatdev.uz",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://water-management.sifatdev.uz",
+]
