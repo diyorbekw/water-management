7c250a691485dfcedac3f8387d82ddcff43f635f
Diyorbek <diyorbek0143@gmail.com>
2026-01-10 18:51:34 +0500

    commit

--- config/settings.py
+++ config/settings.py
@@ -173,7 +173,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://water-management.sifatdev.uz",
-    "https://aquaflow-six.vercel.appg"
+    "https://aquaflow-six.vercel.app"
 ]
  
 CSRF_TRUSTED_ORIGINS = [
